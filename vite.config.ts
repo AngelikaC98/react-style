34b5ec3ef34694ea23c1094a63056b260de51cf4
Angelika Ciuchcińska <angelikaciuchcinska@Angelikas-MacBook-Air.local>
2024-12-11 20:26:21 +0000

    Tried to fixed some error

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+   base: "/react-style/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      base: "/react-style/"
     },
   },
 })
